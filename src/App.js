baaa89ef789076adb013298741aea9929091d364
natatgh <natatgh@gmail.com>
2023-08-24 18:40:26 -0300

    Just another version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="AppName">AppName</div>
+      
+      <div id="email">
+        <input type="email" placeholder="Enter your email" />
+      </div>
+      
+      <div id="password">
+        <input type="password" placeholder="Enter your password" />
+      </div>
+      
+      <div className="forgot_password">
+        <a href="#forgot-password">Forgot password?</a>
+      </div>
+      
+      <div id="enter_button">
+        <button type="submit">ENTER</button>
+      </div>
+
+      <div className="options">
+        {/* Sem conteúdo específico definido */}
+        or login with
+      </div>
+      
+      <div id="other_options">
+        <button className="social-button facebook">
+          <img src="/public/img/facebook-logo.png" alt="Login with Facebook" />
+        </button>
+        <button className="social-button google">
+          <img src="/public/img/google-logo.png" alt="Login with Google" />
+        </button>
+        <button className="social-button apple">
+          <img src="/public/img/apple-logo.png" alt="Login with Apple ID" />
+        </button>
+      </div>
+      
+      <div id="not_have_account">
+        Don't have an account? <a href="#register">Register now</a>
+      </div>
     </div>
   );
 }
