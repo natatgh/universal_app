ea6eb78eab6ae2bdc27d4ff437ca2736f3f3c043
natatgh <natatgh@gmail.com>
2023-08-24 18:43:40 -0300

    Just another version v2

--- src/App.js
+++ src/App.js
@@ -2,48 +2,54 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <div className="AppName">AppName</div>
-      
-      <div id="email">
-        <input type="email" placeholder="Enter your email" />
-      </div>
-      
-      <div id="password">
-        <input type="password" placeholder="Enter your password" />
-      </div>
-      
-      <div className="forgot_password">
-        <a href="#forgot-password">Forgot password?</a>
-      </div>
-      
-      <div id="enter_button">
-        <button type="submit">ENTER</button>
-      </div>
+    return (
+        <div className="App">
+            <div className="AppName">
+                <img src="/img/logo-geral.png" alt="Foodie logo" style={{ width: "50px", marginRight: "10px" }} />
+                <strong>Foodie</strong>
+            </div>
 
-      <div className="options">
-        {/* Sem conteúdo específico definido */}
-        or login with
-      </div>
-      
-      <div id="other_options">
-        <button className="social-button facebook">
-          <img src="/public/img/facebook-logo.png" alt="Login with Facebook" />
-        </button>
-        <button className="social-button google">
-          <img src="/public/img/google-logo.png" alt="Login with Google" />
-        </button>
-        <button className="social-button apple">
-          <img src="/public/img/apple-logo.png" alt="Login with Apple ID" />
-        </button>
-      </div>
-      
-      <div id="not_have_account">
-        Don't have an account? <a href="#register">Register now</a>
-      </div>
-    </div>
-  );
+            <div className="input-field" id="email-field">
+                <input type="email" placeholder=" " id="email-input" required />
+                <label htmlFor="email-input">Enter your email</label>
+            </div>
+
+            <div className="input-field" id="password-field">
+                <input type="password" placeholder=" " id="password-input" required />
+                <label htmlFor="password-input">Enter your password</label>
+            </div>
+
+            <div className="forgot_password">
+                <a href="#forgot-password">Forgot password?</a>
+            </div>
+
+            <div id="enter_button">
+                <button type="submit"><strong>SIGN UP</strong></button>
+            </div>
+
+            <div className="options">
+                <li></li>
+                or login with
+                <li></li>
+            </div>
+
+            <div id="other_options">
+                <button className="social-button facebook">
+                    <img src="/img/facebook-logo.png" alt="Login with Facebook" />
+                </button>
+                <button className="social-button google">
+                    <img src="/img/google-logo.png" alt="Login with Google" />
+                </button>
+                <button className="social-button apple">
+                    <img src="/img/apple-logo.png" alt="Login with Apple ID" />
+                </button>
+            </div>
+
+            <div id="not_have_account">
+                Don't have an account? <a href="#register">Register now</a>
+            </div>
+        </div>
+    );
 }
 
 export default App;
